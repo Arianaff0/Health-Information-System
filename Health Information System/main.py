89a62fcb571bcdf774e5dc1824e144c3b59dfe2e
Ariana <133261064+Arianaff0@users.noreply.github.com>
2024-12-30 19:45:43 -0500

    Update main.py

--- Health Information System/main.py
+++ Health Information System/main.py
@@ -1,4 +1,4 @@
-# Developed by: Ariana Feng
+# Developed by: Ariana F
 # Date: March 28, 2023
 # Desc: A Health Information System program. The system reads patient data from a plain-text file seperated by commas.
 #       New patient data can be added and data can be deleted. Information includes: Patient ID, date of visit, body
